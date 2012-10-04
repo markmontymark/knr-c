61d18b95fc4d037ae28b24e27c700488b8c367e1
markmontymark <markmontymark@yahoo.com>
2012-10-04 15:37:39 -0700

    Cleans up Chapter 1 exercises

--- src/1-4.c
+++ src/1-4.c
@@ -3,17 +3,29 @@
 
 const char * USAGE = "Write Celsius to Farenheit";
 
-void impl( );
+void impl( int argc, char ** argv );
+float c2f( float c);
 
 int main( int argc, char ** argv )
 {
 	char * progname = argv[0];
 	get_progname(progname);
 	usage(USAGE,progname);
-	impl();
+	impl(argc,argv);
 	return 0;
 }
 
-void impl( )
+void impl( int argc, char ** argv)
 {
+	float temp;
+	for(int i = 1; i < argc; i++)
+	{
+		temp = atof(argv[i]);
+		printf("%.2f C = %.2f F\n", temp, c2f(temp));
+	}
+}
+
+float c2f( float c)
+{
+	return c * ((float)9/5) + 32;
 }
