316e30da158e8801b02533bee6d897a1640d1614
Mark Koopman <markmontymark@yahoo.com>
2013-05-19 17:21:05 -0700

    Adding 8-1 impl

--- src/8-1.c
+++ src/8-1.c
@@ -6,23 +6,69 @@
 #include <string.h>
 #include <errno.h>
 #include "common.h"
+#include <fcntl.h>
 
-const char * USAGE = "Rewrite cat with read/write/open/close instead of standard library equivalents. Which is faster, my own cat or the real cat?";
+const char * USAGE = "Rewrite cat with read/write/open/close instead of standard library equivalents. Which is faster, my own cat or the real cat?\n";
 
-void impl( );
+int _cat( int fd_in);
+int cat( char * inpath);
+
+const int MAX_READ = 8192; // get page size w/ a syscall
 
 int main( int argc, char ** argv )
 {
-	printf("%s\n",USAGE);
-	impl();
+	size_t USAGE_LEN = strlen( USAGE );
+	if( write(STDOUT_FILENO, USAGE, USAGE_LEN ) == -1 )
+	{
+		fprintf(stderr,"Error writing %s: %s\n", USAGE, strerror(errno));
+		return errno;
+	}
+	if( argc < 2 )
+		return _cat(STDIN_FILENO);
+
+	int retval;
+	for(int i = 1; i < argc; i++)
+		if( (retval = cat( argv[i] )) )
+			return retval;
 	return 0;
 }
 
-void impl( )
+int cat( char * inpath )
 {
+	int fd_in;
+	if( (fd_in = open(inpath,O_RDONLY,0)) == -1  )
+	{
+		fprintf(stderr,"Error opening %s file for read: %s\n",inpath, strerror(errno));
+		return errno;
+	}
+	_cat( fd_in );
+	if( close( fd_in ) )
+	{
+		fprintf(stderr,"Error closing %s file for read: %s\n",inpath, strerror(errno));
+		return errno;
+	}
+	return 0;
 }
 
-void my_cat(char * path)
-{
-}
 
+int _cat( int fd_in )
+{
+	ssize_t bytesRead;
+	ssize_t bytesWritten;
+	char buf[MAX_READ];
+	while(  (bytesRead = read(fd_in, buf, MAX_READ) ) )
+	{
+		if(  ( bytesWritten = write( STDOUT_FILENO, buf, bytesRead)) == -1 )
+		{
+			fprintf(stderr,"Error writing %s: %s\n", buf, strerror(errno));
+			return errno;
+		}
+	}
+	if( bytesRead == -1 )
+	{
+		fprintf(stderr,"Error reading from fd %d: %s\n", fd_in, strerror(errno));
+		return errno;
+	}
+	
+	return 0;
+}
