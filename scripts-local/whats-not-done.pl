50ce2f9fa3b078d62eac876b942d1df7081a9942
Mark Koopman <markmontymark@yahoo.com>
2013-05-19 17:47:59 -0700

    Added scripts-local/whats-not-done.pl to poll test file for test output that is just the explanation, assuming every impl file has to at least print out (and test) something

---
+++ scripts-local/whats-not-done.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use JSON::XS;
+use File::Slurp;
+
+my $test_cfg_file = shift; #
+$test_cfg_file = 'test/ctest.json' unless defined $test_cfg_file;
+
+my $cfg = JSON::XS::decode_json( File::Slurp::read_file($test_cfg_file));
+
+&whats_not_done( $cfg );
+exit;
+
+
+sub whats_not_done
+{
+	my($cfg) = shift;
+	for( sort keys %$cfg )
+	{
+		print "$_\n" unless 
+			(exists $cfg->{$_}->{expected}  && $cfg->{$_}->{expected} =~ /\n/mg ) ||
+			(exists $cfg->{$_}->{regex_expected}  && $cfg->{$_}->{regex_expected} =~ /(?:\\s|\n)/mg );
+	}	
+}
+
