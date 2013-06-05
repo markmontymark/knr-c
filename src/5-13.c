c8d288801eb5986aed242f61dc6dc9d5c79d7e38
Mark Koopman <markmontymark@yahoo.com>
2013-06-05 10:27:40 -0700

    Adding 5-13 tail impl

--- src/5-13.c
+++ src/5-13.c
@@ -1,37 +1,107 @@
 
 // This file auto-generated on Tue Aug 21 13:50:45 2012
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include "common.h"
+#include <assert.h>
 
-const char * USAGE = "Write tail(int n) to print last n lines...also how about allowing for tail -f ?";
+const char * USAGE = "Write tail -n to print last n lines of input";
 
-void impl( );
-int tail( const char * path, int num_lines );
+
+int tail( char * path, int num_lines );
 
 int main( int argc, char ** argv )
 {
 	printf("%s\n",USAGE);
-	if( argc < 2 )
-		return 0;
-	const char * path = argv[1];
-	int num_lines = atoi(argv[2]);
-	tail( path, num_lines );
+	int num_lines = 10;
+	char ** orig_argv = argv;
+	int orig_argc = argc;
+	if(argc < 2)
+	{
+		printf("Usage: tail [-n] file1 ... fileN\n");
+		return -1;
+	}
+	while(--argc)
+	{
+		if( (*(++argv))[0] == '-' )
+		{
+			char * opt = argv[0];
+			int n = atoi( opt+1 );
+			if(n > 0)
+				num_lines = n;
+		}
+	}
+
+	argc = orig_argc;
+	argv = orig_argv;
+	while( --argc )
+		if( (*(++argv))[0] != '-' )
+			tail( *argv, num_lines );
 	return 0;
 }
 
-int tail( const char * path, int num_lines )
+
+int tail( char * path, int num_lines )
 {
-	FILE * f_in;
-	if( (f_in = fopen(path, "rb")) == NULL )
+	FILE * fp;
+	if( (fp = fopen(path,"rb")) == NULL )
 	{
 		perror(path);
-		return errno;
+		return -1;
 	}
-	if( f_in != NULL )
-		fclose(f_in);
+
+	int bufsize = 1024;
+	char buf[bufsize];
+	int c;
+	int num_lines_found = 0;
+	long pos;
+	if( (pos = lseek(fp->_fileno,0L,SEEK_END)) == -1)
+	{
+		fprintf(stderr, "Error, first fseek of %s: %s\n",path,strerror(errno));
+		return -1;
+	}
+	long endpos = ftell(fp);
+	long seekpos = -1 * bufsize;
+	char chars[bufsize];
+	int bytesRead;
+	char * charp;
+	//printf("fseek first new pos %li endpos %li\n",pos,endpos);
+
+	while( seekpos * -1 < endpos ) 
+	{
+		fseek(fp, seekpos , SEEK_END);
+		//printf("after an fseek(%li), pos is %li\n",seekpos, ftell(fp));
+		pos = 0;
+		//printf("fseek to new pos %li endpos %li seekpos %li\n",pos,endpos,seekpos);
+		bytesRead = 0;
+		charp = chars;
+		while( (bytesRead += read(fp->_fileno,charp,bufsize - bytesRead)) < bufsize )
+		{
+			//printf("read %d bytes\n",bytesRead );
+			charp += bytesRead;
+		}
+		for(int i = bytesRead - 1; i > -1; i--)
+		{
+			if( (c = chars[i]) != '\n')
+				continue;
+			if( ++ num_lines_found == num_lines )
+			{
+				//printf("found %d new lines, need to position stream with endpos %li, seekpos %li, bytesRead %d, i %d\n",num_lines, endpos, seekpos, bytesRead, i);
+				fseek(fp, (seekpos + i), SEEK_END);
+				goto BREAKOUT;
+			}
+				
+		}
+		seekpos += (-1 * bufsize);
+	}
+	BREAKOUT:
+	;
+	while( ( c = fgetc(fp)) != EOF )
+		printf("%c",c);
+
 	return 0;
 }
+
